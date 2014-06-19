48106e347046c2f4a4e8bae0a0e42c480f3db98b
kavyasukumar <kavya.sukumar@hotmail.com>
2014-06-19 18:55:11 -0400

    rail structure

---
+++ www/js/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+	resetLayout();
+	var throttledResize = _.throttle(resetLayout, 100);
+	$(window).resize(throttledResize);
+});
+
+function resetLayout()
+{
+	// calculate cover height
+	$bgheight=$(window).height()-$('#undernav').height();
+	$('.cover').css('height', $bgheight+'px');
+}
