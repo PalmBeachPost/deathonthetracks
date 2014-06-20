72a70725e4bb8cfaa8b68cc90851a1d0bfec487f
kavyasukumar <kavya.sukumar@hotmail.com>
2014-06-20 11:59:36 -0400

    shivs added for html5 and medley css hr hacks

--- www/js/script.js
+++ www/js/script.js
@@ -1,13 +1,28 @@
 $(document).ready(function(){
-	resetLayout();
-	var throttledResize = _.throttle(resetLayout, 100);
+	
+	MedleyShiv();
+	MyHTML5shiv();
+	calcLayout();	
+	var throttledResize = _.throttle(calcLayout, 100);
 	$(window).resize(throttledResize);
+
+	skrollr.init();
 });
 
-function resetLayout()
+function calcLayout()
 {
 	// calculate cover height
 	$bgheight=$(window).height()-$('#undernav').height();
 	$('.cover').css('height', $bgheight+'px');
-	skrollr.init();
+}
+
+function MyHTML5shiv()
+{
+	 $('div#map').attr("data-10-top","position:inherit;top:0%")
+	 	.attr("data-top","position:fixed;top:20%;width:27%")
+	 	.attr("data-1-top","width:100%");
+}
+
+function MedleyShiv(){
+	$('hr.double-rule').parent().addClass("forcefullwidth");
 }
