74ce0f4eb1ccef583ad2ea16fda77ada0efc938b
kavyasukumar <kavya.sukumar@hotmail.com>
2014-06-20 14:40:33 -0400

    fixing mapscroll

--- www/js/script.js
+++ www/js/script.js
@@ -6,19 +6,25 @@ $(document).ready(function(){
 	var throttledResize = _.throttle(calcLayout, 100);
 	$(window).resize(throttledResize);
 
+	//var throttledScroll = _.throttle(hideMap, 1000);
+
 	skrollr.init();
 });
 
 function calcLayout()
 {
 	// calculate cover height
-	$bgheight=$(window).height()-$('#undernav').height();
+	$bgheight=$(window).height()-0;
 	$('.cover').css('height', $bgheight+'px');
 }
 
 function MyHTML5shiv()
 {
-	 $('div#map').attr("data-10-top","position:inherit;top:0%")
+	/* $('div#map').attr("data-10-top","position:inherit;top:0%")
+	 	.attr("data-top","position:fixed;top:20%;width:27%")
+	 	.attr("data-1-top","width:100%");*/
+
+	 	 $('div#map').attr("data-10-top","position:inherit;top:0%")
 	 	.attr("data-top","position:fixed;top:20%;width:27%")
 	 	.attr("data-1-top","width:100%");
 }
@@ -26,3 +32,9 @@ function MyHTML5shiv()
 function MedleyShiv(){
 	$('hr.double-rule').parent().addClass("forcefullwidth");
 }
+
+/*function hideMap(){
+	var currScroll = $(window).scrollTop() + $(window).height();
+	var endOfStoryScroll = $('#content-section').offset().bottom
+	if('#content-section')
+}*/
