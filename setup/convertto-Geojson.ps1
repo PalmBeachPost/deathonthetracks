f9922deeac384b9835ebc4fd83d5bd2989b2b3af
kavyasukumar <kavya.sukumar@hotmail.com>
2014-06-23 18:28:32 -0400

    scripting a geojson conversion

---
+++ setup/convertto-Geojson.ps1
@@ -0,0 +1,58 @@
+param{
+	$csvfile=""
+	$jsonfile="" 
+}
+Add-Type @'
+public class Geometry{
+	public string type ="Point";
+	public string coordinates;
+}
+public class Props{
+	public string caseno;
+	public string date;
+	public int year;
+	public string name;
+	public string sex;
+	public string race;
+	public string age;
+	public string track;
+	public string type;
+	public string location;
+	public string impairment;
+}
+public class Features{
+	public string type="Feature";
+	public Geometry geometry;
+	public Props properties;
+}
+public class Collection{
+	public string type="FeatureCollection";
+	public Features[] features;
+}
+'@
+$collection = new-object Collection
+
+Import-csv $file| 
+foreach-object {
+	$feature= new-object Features
+	$feature.geometry= new-object Geometry
+	$feature.geometry.coordinates = "["+$_.coordinates+"]"
+	
+	$feature.properties = new-object Props
+	$feature.properties.caseno = $_.case
+	$feature.properties.date = $_.date
+	$feature.properties.year =$_.year
+	$feature.properties.name =$_.name
+	$feature.properties.sex = $_.sex
+	$feature.properties.race = $_.race
+	$feature.properties.age= $_.age
+	$feature.properties.track = $_.track
+	$feature.properties.type = $_.type
+	$feature.properties.location =$_.location
+	$feature.properties.impairment = $_.impairment
+
+	$collection.features+=$feature
+	
+}
+
+convertto-geojson $collection -depth 3 | out-file $jsonfile
