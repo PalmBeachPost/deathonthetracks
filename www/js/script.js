ea2163c14174134b391a5c05695b760d0011e68e
kavyasukumar <kavya.sukumar@hotmail.com>
2014-06-26 09:50:11 -0400

    adding more white spcae. decluttering. and all that good stuff

--- www/js/script.js
+++ www/js/script.js
@@ -15,19 +15,21 @@ function calcLayout()
 	// calculate cover height
 	$bgheight=$(window).height()-0;
 	$('.cover').css('height', $bgheight+'px');
-	$creditHeight= $bgheight*.95;
-	$('#credits').css('top',$creditHeight+'px')
 
-	/*var tabWidth = ($('#graphics ul').width()/4)*.95;
-	$('#graphics li').css('width', tabWidth+'px');*/
+	//calculate where to place photo credits
+	$creditHeight= $bgheight-$('#credits').height();
+	$('#credits').css('top',$creditHeight+'px');
+
+	//calculate width of photo caption for leonor
+	//$('#leonorimg .caption').css('width',$('#leonorimg img').width()+'px');
 }
 
 function MyHTML5Shiv()
 {
 	$('div#map').attr("data-anchor-target","#mapbuddy")
 		.attr("data-top-top","position:inherit;bottom:!10%;;width:!100%")
-	 	.attr("data--50-top","position:fixed;bottom:!7%;width:!27%")
-	 	.attr("data-bottom","position:absolute;bottom:!0%;width:!30%;");
+	 	.attr("data--50-top","position:fixed;bottom:!7%;width:!24%")
+	 	.attr("data-bottom","position:absolute;bottom:!0.5%;width:!30%;");
 
 	 $('a.left.carousel-control').attr("data-slide","prev");
 	 $('a.right.carousel-control').attr("data-slide","next");
